c824e5b5caddfadaa3ac3f85c3fb156c44129288
odhiambovinogola <odhiambovinogola@gmail.com>
2023-09-07 16:27:16 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * Description: prints "Programming is like building a multilingual puzzle"
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
