62dcf20d174f4a22de6758f0a073951a054bfc01
odhiambovinogola <odhiambovinogola@gmail.com>
2023-09-10 01:39:02 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- printing alphabets in lowercase in reverse
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet = 'z';
+
+	while (alphabet >= 'a')
+	{
+		putchar(alphabet);
+		alphabet--;
+	}
+
+	putchar('\n');
+	return (0);
+}
