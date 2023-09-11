caa07c44c88426199efc6b4c09491afa6ac859e9
odhiambovinogola <odhiambovinogola@gmail.com>
2023-09-11 11:56:26 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible different combinations of two digits
- * Return - Always 0 (Success)
+ * main - Prints numbers between 00 to 89.
+ * Return: Always 0 (Success)
  */
 
 int main(void)
