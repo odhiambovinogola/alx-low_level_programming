e04ce373597598d67b742021bf3be134a518fea3
odhiambovinogola <odhiambovinogola@gmail.com>
2023-09-11 12:36:01 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 00 to 99.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x < 100; x++)
+	{
+		for (y = 0; y < 100; y++)
+		{
+			putchar(x / 10 + 48);
+			putchar(x % 10 + 48);
+			putchar(' ');
+			putchar(y / 10 + 48);
+			putchar(y % 10 + 48);
+			if (x != 98 || y != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
