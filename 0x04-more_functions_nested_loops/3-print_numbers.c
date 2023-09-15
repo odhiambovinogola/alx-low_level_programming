c571d77341378d05ee1fc143970d3e5e781ee74d
odhiambovinogola <odhiambovinogola@gmail.com>
2023-09-15 15:38:51 -0700

    print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers between 0 to 9
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
