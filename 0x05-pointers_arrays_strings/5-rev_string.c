953526c538d52a8c65fb241064c54812b0b9bd77
odhiambovinogola <odhiambovinogola@gmail.com>
2023-09-19 00:53:22 +0300

    rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,20 +8,19 @@
 
 void rev_string(char *s)
 {
-	int v;
-	int count = 0;
+int v;
+int count = 0;
 
-	for (v = 0; s[v] != '\0'; v++)
-	{
-		count++;
-		for (v = 0; v < count / 2; v++)
-		{
-			char k;
+for (v = 0; s[v] != '\0'; v++)
+	count++;
+for (v = 0; v < count / 2; v++)
+{
+char k;
+k = s[v];
+	s[v] = s[count - 1 - v];
+	s[count - 1 - v] = k;
+}
 
-			k = s[v];
-			s[v] = s[count - 1 - v];
-			s[count - 1 - v] = k;
-		}
-	}
+
 }
 
