cac08a89d78ccd5fce5bb3f6e8918e28d50850cf
odhiambovinogola <odhiambovinogola@gmail.com>
2023-09-21 21:30:19 +0300

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strcat - concatenates two strings
  * @dest: destination
@@ -9,15 +10,21 @@
 char *_strcat(char *dest, char *src)
 {
 	int i;
-	int k = 0;
+	int k;
 
-	while (dest[k] != '\0')
+	i = 0;
+	while (dest[i] != '\0')
 	{
+		i++;
+	}
+	k = 0;
+	while (src[k] != '\0')
+	{
+		dest[i] = src[k];
+		i++;
 		k++;
 	}
-	for (i = 0 ; i < k && src[i] != '\0' ; i++)
-		dest[k + i] = src[i];
-	dest[k + i] = '\0';
+	dest[i] = '\0'
 
 	return (dest);
 }
