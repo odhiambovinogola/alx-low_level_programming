60e60599f7198a7a8846c093397bf45b1bbeced9
odhiambovinogola <odhiambovinogola@gmail.com>
2023-09-21 23:04:51 +0300

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,19 +9,20 @@
 char *leet(char *s)
 {
 	int i;
+	int n;
+	char c[] = "aAeEoOtTlL";
+	char h[] = "4433007711";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (n = 0; c[n] != '\0'; n++)
+		{
+			if (s[i] == c[n])
+			{
+				s[i] = h[n];
+			}
+		}
+
 	}
 
 	return (s);
