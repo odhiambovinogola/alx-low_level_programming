2a935453cd21f2af6dc36413393914fb8fecdeb3
odhiambovinogola <odhiambovinogola@gmail.com>
2023-09-28 22:28:12 +0300

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -15,20 +15,20 @@ int _strlen_recursion(char *s)
 }
 
 /**
- * compare - compares each character
+ * comparator - compares each character
  * @s: string
  * @n1: smallest iterator
  * @n2: biggest iterator
  * Return: integer indicating non-match 0, match 1
  */
 
-int compare(char *s, int n1, int n2)
+int comparator(char *s, int n1, int n2)
 {
 	if (*(s + n1) == *(s + n2))
 	{
 		if (n1 == n2 || n1 == n2 + 1)
 			return (1);
-		return (0 + compare(s, n1 + 1, n2 - 1));
+		return (0 + comparator(s, n1 + 1, n2 - 1));
 	}
 	return (0);
 }
