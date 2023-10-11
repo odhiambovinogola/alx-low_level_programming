c58b5bf3fc518b5b38e60fcf00e1b7da807124a9
odhiambovinogola <odhiambovinogola@gmail.com>
2023-10-11 23:19:34 +0300

    int_index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -18,8 +18,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 
 	for (i = 0; i < size; i++)
+	{
 		res = cmp(array[i]);
-	if (res == 1)
-		return (i);
+		if (res == 1)
+			return (i);
+	}
 	return (-1);
 }
