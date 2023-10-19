6a4b6b1ab2e84c5d6bc5fa0129f12bed3c393cae
odhiambovinogola <odhiambovinogola@gmail.com>
2023-10-20 01:30:13 +0300

    primary

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * primary - Print before the main function.
+ *
+ * Return: void
+ */
+
+__attribute__((constructor))
+void primary(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("bore my house upon my back!\n");
+}
