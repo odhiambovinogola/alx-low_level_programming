e0bd6b456fd546a1a0736067f8f2e405f3980874
odhiambovinogola <odhiambovinogola@gmail.com>
2023-10-20 00:32:56 +0300

    list_len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - Returns the number of elements in a linked list_t list.
+ * @h: A pointer to the head of the list_t list.
+ *
+ * Return: The number of elements in the list.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t counter;
+
+	counter = 0;
+	while (h != NULL)
+	{
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
