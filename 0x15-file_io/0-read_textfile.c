ee3abf43353b467ba128892b7feddac0ccc49c02
odhiambovinogola <odhiambovinogola@gmail.com>
2023-10-30 23:27:55 +0300

    read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,48 +2,41 @@
 
 /**
  * read_textfile - Reads text file and prints it to the POSIX standard output
- * @filename: Name of the file to read
- * @letters: Number of letters it should read and print
+ * @filename: The name of the file.
+ * @letters: The number of letters it should read and print.
  *
- * Return: Actual number of letters it could read and print, 0 on failure
+ * Return: The actual number of letters it could read and print.
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, bytes_read, bytes_written;
-	char *buffer;
+	int file_descriptor;
+	char *text_buffer;
+	ssize_t bytes_read, bytes_written;
 
 	if (filename == NULL)
 		return (0);
 
-	buffer = malloc(letters);
-	if (buffer == NULL)
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-	{
-		free(buffer);
+	text_buffer = malloc(sizeof(char) * letters + 1);
+	if (text_buffer == NULL)
 		return (0);
-	}
 
-	bytes_read = read(fd, buffer, letters);
+	bytes_read = read(file_descriptor, text_buffer, letters);
 	if (bytes_read == -1)
-	{
-		free(buffer);
-		close(fd);
 		return (0);
-	}
 
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1 || bytes_written != bytes_read)
-	{
-		free(buffer);
-		close(fd);
+	text_buffer[letters] = '\0';
+	close(file_descriptor);
+
+	bytes_written = write(STDOUT_FILENO, text_buffer, bytes_read);
+	if (bytes_written == -1)
 		return (0);
-	}
 
-	free(buffer);
-	close(fd);
-	return (bytes_written);
+	free(text_buffer);
+
+	return (bytes_read);
 }
